5b48a41a1183e3abb8060db6d064c37c0e2cc7d5
Puleeno Nguyen <puleeno@gmail.com>
2020-06-22 08:17:20 +0700

    Rename Spider to Scrapy

---
+++ src/Scrapy.php
@@ -0,0 +1,9 @@
+<?php
+namespace Ramphor\Scrapy;
+
+class Scrapy
+{
+    public function __construct(Adapter $adapter)
+    {
+    }
+}
--- src/Spider.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Ramphor\Spider;
-
-
-class Spider {
-}
