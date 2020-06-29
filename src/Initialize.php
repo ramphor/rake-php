35e1aff19828254605750c1469a719109f1c870f
Puleeno Nguyen <puleeno@gmail.com>
2020-06-29 20:33:41 +0700

    Create new table and add teeth to rake

--- src/Initialize.php
+++ src/Initialize.php
@@ -23,6 +23,7 @@ class Initialize
     {
         $this->driver->createDbTable('rake_crawled_urls', "`ID` bigint(20) NOT NULL AUTO_INCREMENT,
             `url` text NOT NULL,
+            `teeth_id` VARCHAR(255) NOT NULL,
             `crawled` tinyint(4) NOT NULL,
             `retry` tinyint(4) NOT NULL DEFAULT '0',
             `created_at` timestamp NULL DEFAULT NULL,
@@ -33,6 +34,7 @@ class Initialize
             `source_id` bigint(20) NOT NULL COMMENT 'The resource #ID contain resource_id'");
 
         $this->driver->createDbTable('rake_resources', "`ID` bigint(20) NOT NULL AUTO_INCREMENT,
+            `teeth_id` VARCHAR(255) NOT NULL,
             `guid` text NOT NULL,
             `sha256_hash` char(64) NOT NULL,
             `new_guid` text,
@@ -41,6 +43,13 @@ class Initialize
             `retry` int(11) NOT NULL DEFAULT '0',
             `created_at` timestamp NULL DEFAULT NULL,
             `updated_at` timestamp NULL DEFAULT NULL,
+            PRIMARY KEY (`ID`)");
+
+        $this->driver->createDbTable('rake_preprocessors', "`ID` int(11) NOT NULL,
+            `teeth_id` varchar(255) NOT NULL,
+            `preprocessor_id` varchar(255) NOT NULL,
+            `options` longtext NOT NULL,
+            `last_execute` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
             PRIMARY KEY (`ID`)");
     }
 }
