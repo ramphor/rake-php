4e0a0ec80b667fea17d2cbfbc174fe295b93dd67
Puleeno Nguyen <puleeno@gmail.com>
2020-06-29 22:38:16 +0700

    Add iterator methods

--- src/Parsers/HTML/Parser.php
+++ src/Parsers/HTML/Parser.php
@@ -2,7 +2,27 @@
 namespace Ramphor\Rake\Parsers\HTML;
 
 use Ramphor\Rake\Abstracts\AbstractParser;
+use Ramphor\Rake\DataSource\FeedItem;
 
 class Parser extends AbstractParser
 {
+    public function key()
+    {
+    }
+
+    public function current(): FeedItem
+    {
+    }
+
+    public function rewind()
+    {
+    }
+
+    public function next()
+    {
+    }
+
+    public function valid()
+    {
+    }
 }
