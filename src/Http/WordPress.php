5b87d610ab741575c97d2ff061575098989f7727
Puleeno Nguyen <puleeno@gmail.com>
2020-06-29 23:52:06 +0700

    Can download sitemap in preprocessor

--- src/Http/WordPress.php
+++ src/Http/WordPress.php
@@ -1,8 +1,26 @@
 <?php
 namespace Ramphor\Rake\Http;
 
+use Requests;
 use Ramphor\Rake\Abstracts\AbstractHttpClient;
 
 class WordPress extends AbstractHttpClient
 {
+    public function request($method, $uri = '', $options = [])
+    {
+        $headers = [];
+        if (isset($options['headers'])) {
+            $headers = $options['headers'];
+            unset($options['headers']);
+        }
+
+        $data = [];
+        if (isset($options['form_params'])) {
+            $data = $options['form_params'];
+            unset($options['form_params']);
+        }
+
+        $response = Requests::request($uri, $headers, $data, strtoupper($method), $options);
+        return $response->body;
+    }
 }
--- src/Preprocessors/Sitemap/Sitemap.php
+++ src/Preprocessors/Sitemap/Sitemap.php
@@ -1,8 +1,12 @@
 <?php
 namespace Ramphor\Rake\Preprocessors\Sitemap;
 
+use SimpleXMLElement;
+
 class Sitemap extends Base
 {
+    protected $httpClient;
+
     protected $namespaces = [
         'xmlns' => 'http://www.sitemaps.org/schemas/sitemap/0.9',
         'xmlns:image' => 'http://www.google.com/schemas/sitemap-image/1.1'
@@ -10,5 +14,7 @@ class Sitemap extends Base
 
     public function execute()
     {
+        $xml_str = $this->httpClient->request('GET', $this->url);
+        $xml_sitemap = new SimpleXMLElement($xml_str);
     }
 }
