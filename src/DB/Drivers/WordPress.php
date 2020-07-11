ce444227f08372b4502c50912d090b4106afd540
Puleeno Nguyen <puleeno@gmail.com>
2020-07-11 21:45:01 +0700

    Define WordPress DB driver

---
+++ src/DB/Drivers/WordPress.php
@@ -0,0 +1,27 @@
+<?php
+namespace Ramphor\Rake\DB\Drivers;
+
+use Ramphor\Rake\Abstracts\Driver;
+use Ramphor\Rake\Constracts\SQLQueryBuilder;
+
+class WordPress extends Driver
+{
+    public const DRIVER_NAME = 'wordpress';
+
+    protected $dbInstance;
+
+    public function __construct()
+    {
+        $this->dbInstance = $GLOBALS['wpdb'];
+    }
+
+    public function getName()
+    {
+        return self::DRIVER_NAME;
+    }
+
+    public function query(SQLQueryBuilder $query)
+    {
+        return $this->dbInstance->query((string) $query);
+    }
+}
