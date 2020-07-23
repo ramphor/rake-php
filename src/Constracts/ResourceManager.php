631c0defd8f4f4f847fbf146dbd048438099ce56
Puleeno Nguyen <puleeno@gmail.com>
2020-07-23 23:04:01 +0700

    Fix bug define getFilesFromDatabase method

--- src/Constracts/ResourceManager.php
+++ src/Constracts/ResourceManager.php
@@ -5,12 +5,13 @@ namespace Ramphor\Rake\Constracts;
 use Ramphor\Rake\ProcessResult;
 use Ramphor\Rake\Resource;
 use Ramphor\Rake\Abstracts\ResourceManager as ResourceManagerAbstract;
+use Ramphor\Rake\Abstracts\Tooth;
 
 interface ResourceManager
 {
     public function createFromResult(ProcessResult $result): ResourceManagerAbstract;
 
-    public function getFilesFromDatabase(): ResourceManagerAbstract;
+    public function getFilesFromDatabase(Tooth $tooth): ResourceManagerAbstract;
 
     public function find(int $resouceId): ? Resource;
 }
