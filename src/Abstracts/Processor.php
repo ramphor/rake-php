0de64ecd1126693ca95a018f6983e4ba6c3a4d01
Puleeno Nguyen <puleeno@gmail.com>
2020-08-05 16:00:48 +0700

    Clean content before import to database

--- src/Abstracts/Processor.php
+++ src/Abstracts/Processor.php
@@ -2,6 +2,9 @@
 namespace Ramphor\Rake\Abstracts;
 
 use Ramphor\Rake\Link;
+use PHPHtmlParser\Dom;
+use PHPHtmlParser\Dom\HtmlNode;
+use PHPHtmlParser\Dom\Tag;
 use Ramphor\Rake\Constracts\Processor as ProcessorConstract;
 use Ramphor\Rake\Facades\Client;
 use Ramphor\Rake\Facades\Logger;
@@ -14,7 +17,15 @@ abstract class Processor implements ProcessorConstract
     protected $feedItem;
     protected $importedId;
 
+    protected $cleanContentAttributes = true;
     protected $crawlLinks = false;
+
+    protected static $ignoreAttributes = ['style'];
+    protected static $doNotReplaceTags = ['a', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+    protected static $convertRules = [
+        '[style*="font-weight: bold"]' => 'strong',
+        '[style*="font-style: italic"]' => 'em',
+    ];
 
     public function setId(string $id)
     {
@@ -57,5 +68,79 @@ abstract class Processor implements ProcessorConstract
     public function convertImageUrl($imageUrl)
     {
         return $imageUrl;
+    }
+
+    protected function convertStylesheets($document)
+    {
+        foreach (static::$convertRules as $rule => $tagName) {
+            $htmlNodes = $document->find($rule);
+            if (count($htmlNodes) <= 0) {
+                continue;
+            }
+
+            foreach ($htmlNodes as $htmlNode) {
+                $wrapTag = new HtmlNode(new Tag($htmlNode->tag->name()));
+                foreach ($htmlNode->getAttributes() as $attribute => $value) {
+                    if (in_array($attribute, static::$ignoreAttributes)) {
+                        continue;
+                    }
+                    $wrapTag->setAttribute($attribute, $value);
+                }
+                $newTag = new HtmlNode(new Tag($tagName));
+                foreach ($htmlNode->getChildren() as $children) {
+                    $newTag->addChild($children);
+                }
+                if (in_array($htmlNode->tag->name(), static::$doNotReplaceTags)) {
+                    $wrapTag->addChild($newTag);
+                    $htmlNode->getParent()->replaceChild($htmlNode->id(), $wrapTag);
+                } else {
+                    $htmlNode->getParent()->replaceChild($htmlNode->id(), $newTag);
+                }
+            }
+        }
+        return $document;
+    }
+
+    /**
+     * Remove the attributes in HTML of the post content
+     *
+     * @link https://stackoverflow.com/questions/3026096/remove-all-attributes-from-an-html-tag
+     * @param string $html
+     * @return string
+     */
+    public function removeContentAttributes($document)
+    {
+        foreach (static::$ignoreAttributes as $attribute) {
+            foreach ($document->find(sprintf('[%s]', $attribute)) as $htmlNode) {
+                $htmlNode->tag->removeAttribute($attribute);
+            }
+        }
+        return $document;
+    }
+
+    /**
+     * Cleanup the HTML before import to your system
+     *
+     * @todo Convert styles to strong and em tags.
+     * @todo Remove all attribute of the tags
+     *
+     * @param string $content The post content
+     * @return string The output HTML after cleanup
+     */
+    public function cleanupContentBeforeImport($content)
+    {
+        if (!$this->cleanContentAttributes) {
+            return $content;
+        }
+
+        $document = new Dom();
+        $document->load((string)$content);
+
+        // Convert common stylesheet
+        $document = $this->convertStylesheets($document);
+        $document = $this->removeContentAttributes($document);
+
+        // Return the clean content
+        return $document->innerHtml;
     }
 }
