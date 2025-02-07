5e5952cc4a2f303aebc35ffc3550196a49747a46
Puleeno Nguyen <puleeno@gmail.com>
2025-02-07 08:23:40 +0700

    update missing methods

--- src/Facades/Resources.php
+++ src/Facades/Resources.php
@@ -11,8 +11,19 @@ use Ramphor\Rake\Resource;
  * @method static bool has(string $id)
  * @method static array getResources()
  * @method static void clear()
+ * @method static Resource makeResource(string $id, array $data = [])
+ * @method static void bind(string $abstract, \Closure|string|null $concrete = null)
+ * @method static bool isBound(string $abstract)
+ * @method static mixed resolve(string $abstract)
+ * @method static Resource findParent(int $childId)
+ * @method static int getTotalResources()
+ * @method static Resource findByQuery(\Ramohor\Sql $query, callable $callback = null)
+ * @method static Resource find(int $resourceId)
+ * @method static string generateHash($data, $type)
+ * @method static Resource getFromHash($hash, $type)
+ * @method static void skipLinkByUrl($url, $tooth)
  *
- * @see \Ramphor\Rake\Constracts\ResourceManager
+ * @see \Ramphor\Rake\Abstracts\ResourceManager
  */
 class Resources extends Facade
 {
